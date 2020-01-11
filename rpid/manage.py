e562fc75dea09f27842ba7864b952eb335245897
Sunny Capt <sunny.capt@tuta.io>
2020-01-11 09:46:52 +0300

    Update manage.py

--- rpid/manage.py
+++ rpid/manage.py
@@ -1,15 +1,24 @@
 from rpid.controller import Controller
 import rpid.config
 
-def main():
+def start():
     controller = Controller()
     for thread in controller.get_dumpers():
         thread.start()
     return None
 
+_actions = {
+    'start': start
+}
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "--log":
+    if len(sys.argv) > 1:
+        action = _actions[sys.argv[1]]
+    else:
+        exit(-1)
+        
+    if len(sys.argv) > 2 and sys.argv[2] == "--log":
         config.logging_.config["loggers"]["general"]["handlers"].append("console")
         logging.config.dictConfig(config.logging_.config)
-    main()
+    
+    action()
